7edfedf25658192166eff7830d94f15007086a41
Roger Hughes <roger.hughes@triad.uk.com>
2016-02-09 14:59:47 +0000

    Now compatible with EE3 and also EE2

---
+++ addon.setup.php
@@ -0,0 +1,11 @@
+<?php 
+
+return array(
+    'name'              => 'ImageSizer',
+    'version'           => '3.0.0',
+    'author'            => 'David Rencher, Christian Maloney, Roger Hughes, Stephen Sweetland',
+    'author_url'        => 'https://github.com/meatpaste/imgsizer',
+    'description'       => 'Image Resizer - resizes and caches images',
+    'namespace'         => 'Ellislab\Addons\Imgsizer',
+    'settings_exist'    => FALSE,
+);
