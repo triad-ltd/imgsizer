58159efa6a7f7b6cd642185be47010a037ca74fe
Roger Hughes <roger@clicked.me.uk>
2016-09-23 23:03:57 +0100

    Add missing required files

---
+++ language/english/imgsizer_lang.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+
+	'my_module_module_name'     => 'Imgsizer',
+	'my_module_module_description'  => 'Imgsizer',
+
+	// END
+	''=>''
+);
